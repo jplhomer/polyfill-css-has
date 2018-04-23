a5879ea20c97dccb6d853c05844b9f3461281a82
Josh Larson <jplhomer@gmail.com>
2018-04-23 11:16:43 -0500

    Add comments

--- src/polyfill-css-has.ts
+++ src/polyfill-css-has.ts
@@ -26,6 +26,12 @@ export default function querySelectorAllWithHas(
   return filterNodesInScopeByHasSelector(nodes, hasSelector as string);
 }
 
+/**
+ * Get the inner-selector from the :has() statement.
+ * Returns false if no :has() is present.
+ *
+ * @param selector A CSS selector possibly containing :has()
+ */
 export function getHasInnerSelector(selector: string): string | Boolean {
   const matches = /:has\((.*)\)/.exec(selector);
 
@@ -36,6 +42,12 @@ export function getHasInnerSelector(selector: string): string | Boolean {
   return matches[1];
 }
 
+/**
+ * Get the elements in the resolved scope prior to the :has() statement.
+ *
+ * @param dom       Element
+ * @param selector  Selector
+ */
 export function getNodesInCurrentScope(
   dom: Element | Document,
   selector: string
@@ -45,10 +57,21 @@ export function getNodesInCurrentScope(
   return dom.querySelectorAll(currentScopeSelector);
 }
 
+/**
+ * Grab the top-level scope, immediately to the left of :has()
+ *
+ * @param selector
+ */
 function getCurrentScopeSelector(selector: string): string {
   return selector.slice(0, selector.indexOf(':has('));
 }
 
+/**
+ * Perform the querySelectorAll behavior against the nodes at the top level.
+ *
+ * @param nodes     Filtered nodes from the prior scope.
+ * @param selector  The inner :has() selector
+ */
 export function filterNodesInScopeByHasSelector(
   nodes: NodeList,
   selector: string
